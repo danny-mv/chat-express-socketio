c8586487045a9a4a1105d1c40e746560a5c50bfb
Danny Mejia <dannym.mejia@gmail.com>
2023-06-25 16:42:47 +0200

    feat: SequelizeRoom

---
+++ server/src/Rooms/infrastructure/RoomInstance.ts
@@ -0,0 +1,16 @@
+import { DataTypes, ModelAttributes } from "sequelize";
+
+export const RoomInstance: ModelAttributes = {
+	id: {
+		type: DataTypes.STRING,
+		primaryKey: true,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	userId: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+};
